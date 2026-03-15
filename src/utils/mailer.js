bcceaf905ad862cdd5ed0d862365984563988d8c
Kevin <oooooq40@gmail.com>
2026-03-15 16:14:36 -0500

    correo valido 6

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -18,16 +18,29 @@ const transporter = nodemailer.createTransport({
 
 const enviarVerificacion = async (email, link) => {
 
-  await transporter.sendMail({
-    from: `"Cámara de Comercio - Jipijapa" <${process.env.EMAIL_USER}>`,
-    to: email,
-    subject: "Verifica tu cuenta",
-    html: `
-      <h2>Verificación de cuenta</h2>
-      <p>Haz click para activar tu cuenta:</p>
-      <a href="${link}">Activar cuenta</a>
-    `
-  });
+  try {
+
+    console.log("Intentando enviar correo a:", email);
+    console.log("Link:", link);
+
+    const info = await transporter.sendMail({
+      from: `"Cámara de Comercio - Jipijapa" <${process.env.EMAIL_USER}>`,
+      to: email,
+      subject: "Verifica tu cuenta",
+      html: `
+        <h2>Verificación de cuenta</h2>
+        <p>Haz click para activar tu cuenta:</p>
+        <a href="${link}">Activar cuenta</a>
+      `
+    });
+
+    console.log("Correo enviado:", info.messageId);
+
+  } catch (error) {
+
+    console.error("ERROR MAIL:", error);
+
+  }
 
 };
 
