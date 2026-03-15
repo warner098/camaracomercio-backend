08306016a66f9c005c412024e6038f4f61121d18
Kevin <oooooq40@gmail.com>
2026-03-15 17:28:48 -0500

    correo valido brevo 1

--- src/utils/mailer.js
+++ src/utils/mailer.js
@@ -1,24 +1,40 @@
-const { Resend } = require("resend");
-
-const resend = new Resend(process.env.RESEND_API_KEY);
+const SibApiV3Sdk = require("sib-api-v3-sdk");
 
 const enviarVerificacion = async (email, link) => {
   try {
 
-    console.log("Enviando correo con Resend a:", email);
+    console.log("Enviando correo con Brevo a:", email);
 
-    await resend.emails.send({
-  from: "Cámara de Comercio <mail.camaradecomer.com>",
-  to: email,
-  subject: "Verifica tu cuenta",
-  html: `
-    <h2>Verificación de cuenta</h2>
-    <p>Haz click para activar tu cuenta:</p>
-    <a href="${link}">Activar cuenta</a>
-  `
-});
+    const client = SibApiV3Sdk.ApiClient.instance;
 
-    console.log("Correo enviado correctamente");
+    const apiKey = client.authentications["api-key"];
+    apiKey.apiKey = process.env.BREVO_API_KEY;
+
+    const apiInstance = new SibApiV3Sdk.TransactionalEmailsApi();
+
+    const sendSmtpEmail = new SibApiV3Sdk.SendSmtpEmail();
+
+    sendSmtpEmail.subject = "Verifica tu cuenta";
+    sendSmtpEmail.htmlContent = `
+      <h2>Verificación de cuenta</h2>
+      <p>Haz click para activar tu cuenta:</p>
+      <a href="${link}">Activar cuenta</a>
+    `;
+
+    sendSmtpEmail.sender = {
+      name: "Cámara de Comercio Jipijapa",
+      email: "tuemail@gmail.com"
+    };
+
+    sendSmtpEmail.to = [
+      {
+        email: email
+      }
+    ];
+
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+
+    console.log("Correo enviado correctamente con Brevo");
 
   } catch (error) {
 
